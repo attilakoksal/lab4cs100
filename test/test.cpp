8abd8a26c8276f120ec90ea43af2825970007790
swang433 <swang433@ucr.edu>
2023-04-28 10:36:09 -0700

    first test

--- test/test.cpp
+++ test/test.cpp
@@ -2,8 +2,16 @@
 #include "../include/Triangle.h"
 using shapes::Triangle;
 
+//getPerimeter() getArea() getKind() are bugged
+
 
 TEST(TriangleTests, testPerimeter) {
     Triangle *aTriangle = new Triangle(3,3,3);
     EXPECT_EQ (aTriangle->getPerimeter(),9);
+}
+
+TEST(TriangleTests, testArea)
+{
+    Triangle *aTriangle = new Triangle(3, 3, 3);
+    EXPECT_EQ (aTriangle->getArea(),1.5*sqrt(11.25));
 }
